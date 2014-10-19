2f7de2c8755a840bfcf34b2b6508e86eba0fbccc
Soulou <leo@unbekandt.eu>
2014-10-19 03:05:30 +0200

    Add BuildStackWithSkip

--- errgo_rollbar.go
+++ errgo_rollbar.go
@@ -15,9 +15,17 @@ func BuildStack(err error) rollbar.Stack {
 				Filename: errgoErr.File,
 				Line:     errgoErr.Line,
 			}
-			stack = append(stack, frame)
+			stack = append([]rollbar.Frame{frame}, stack...)
 			err = errgoErr.Underlying()
 		}
 	}
 	return stack
+}
+
+// BuildStackWithSkip concatenates the stack given by the current execution flow
+// and the stack determined by the errgo error
+func BuildStackWithSkip(err error, skip int) rollbar.Stack {
+	errStack := BuildStack(err)
+	execStack := rollbar.BuildStack(skip + 2)
+	return append(errStack, execStack...)
 }
