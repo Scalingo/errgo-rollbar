233adcd05fe2aa08c6f7449c26a5530726f05207
Soulou <leo@unbekandt.eu>
2018-09-28 16:18:07 +0200

    Change rollbar lib from stvp/rollbar to offical lib

--- errgo_rollbar.go
+++ errgo_rollbar.go
@@ -1,12 +1,44 @@
 package errgorollbar
 
 import (
-	"github.com/stvp/rollbar"
+	"strings"
+
+	"github.com/rollbar/rollbar-go"
 	"gopkg.in/errgo.v1"
 )
 
-func BuildStack(err error) rollbar.Stack {
+// Rollbar package expect such error:
+// type CauseStacker interface {
+//   error
+//   Cause() error
+//   Stack() Stack
+// }
+
+var (
+	_ rollbar.CauseStacker = wrappedError{}
+)
+
+type wrappedError struct {
+	err  error
+	msg  string
+	skip int
+}
+
+func (err wrappedError) Error() string {
+	return err.msg
+}
+
+func Wrap(msg string, err error, skip int) wrappedError {
+	return wrappedError{msg: msg, err: err, skip: skip}
+}
+
+func (err wrappedError) Cause() error {
+	return err.err
+}
+
+func (werr wrappedError) Stack() rollbar.Stack {
 	stack := rollbar.Stack{}
+	err := werr.err
 	for err != nil {
 		if errgoErr, ok := err.(*errgo.Err); !ok {
 			break
@@ -19,13 +51,17 @@ func BuildStack(err error) rollbar.Stack {
 			err = errgoErr.Underlying()
 		}
 	}
-	return stack
-}
 
-// BuildStackWithSkip concatenates the stack given by the current execution flow
-// and the stack determined by the errgo error
-func BuildStackWithSkip(err error, skip int) rollbar.Stack {
-	errStack := BuildStack(err)
-	execStack := rollbar.BuildStack(skip + 2)
-	return append(errStack, execStack...)
+	// The result is the concatenation of the stack given by the current
+	// execution flow and the stack determined by the pkg/errors error
+	// Plus we ignore all the intermediate frames from rollbar lib which
+	// is adding something around 4 levels of depth.
+	rawStack := rollbar.BuildStack(werr.skip + 2)
+	execStack := rollbar.Stack{}
+	for _, frame := range rawStack {
+		if !strings.Contains(frame.Filename, "rollbar/rollbar-go") {
+			execStack = append(execStack, frame)
+		}
+	}
+	return append(stack, execStack...)
 }
